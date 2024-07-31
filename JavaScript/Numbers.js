55ab331714d40804843d1edf14ba48c94482a063
Yafet Daniel Haileselassie <japhethdanny@gmail.com>
2024-07-31 10:26:25 +0300

    Numbers

---
+++ JavaScript/Numbers.js
@@ -0,0 +1,43 @@
+var num1 = 10;
+var num2 = 10.9;
+
+console.log(typeof(num1))
+console.log(typeof(num2))
+
+// Addition
+var num3 = 15;
+var sum = num3 + num1 + num2;
+console.log(sum)
+
+// Subtraction
+var diff = num3 - num1;
+console.log(diff)
+
+// Multiplication
+var mul = num3 + num1;
+console.log(mul)
+
+// Division
+var div = num3 / num1;
+console.log(div)
+
+var divByZero = num3/0;
+console.log(divByZero);
+console.log(divByZero);
+
+var mulByString = num3 * 'A';
+console.log(mulByString);
+console.log(typeof(mulByString));
+
+console.log('')
+
+// Practice Problem
+    console.log(0/0) // NaN
+    console.log(0/5) // 0
+    console.log(3*'a') //NaN
+    console.log('a' * 3) // NaN
+    console.log('b' + 4) // Concatenation
+    console.log(4 + 'b') // Concatenation
+    console.log(5 - 'c') // NaN
+    console.log('c' - 5) // NaN
+
