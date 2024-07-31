9b61bc490d376e6fe948672eb84f4e0c0067573b
Yafet Daniel Haileselassie <japhethdanny@gmail.com>
2024-07-31 12:59:08 +0300

    In-Built String Functions Part Two

--- JavaScript/In-Built String Functions.js
+++ JavaScript/In-Built String Functions.js
@@ -47,5 +47,64 @@ console.log('')
     var substringEnd = myStr.substring(myStr.length-12);
     console.log(substringEnd);
 
+console.log('');
+
+var exampleString = "This is Javascript tutorial";
+
+// toUpperCase() - It converts our String -> uppercase characters
+console.log(exampleString.toUpperCase());
+
+// toLowerCase() - It converts our string -> lowercase characters
+console.log(exampleString.toLowerCase());
+
+// concat() - It merges two or more strings
+var firstName = "JavaScript"
+var lastName = "Playground"
+console.log(exampleString.concat(firstName));
+console.log(exampleString.concat(' ', firstName, ' ', lastName));
+
+// We can also use '+' to concat two or more strings
+console.log(firstName + ' ' + lastName + ' ' + exampleString);
+
+// trim() - It removes extra spaces
+var extraSpaceString = '              my string     ';
+console.log(extraSpaceString.trim());
+
+// charAt() -> this take a position as an arg and returns the character at that position
+var charAtExampleString = 'This is my test string';
+console.log(charAtExampleString.charAt(5));
+console.log(charAtExampleString.charAt(11));
+
+// split() - splits our string on the basis of the arguments passed
+var sampleString = 'This is my sample string';
+console.log(sampleString.split(' '));
+
+var sampleString2 = "This,is,my,sample,string";
+console.log(sampleString2.split(','));
+
+var sampleString3 = "This is awesome";
+console.log(sampleString3.split());
+
+console.log('');
+
+// Practice Problem 2
+    var str1 = 'Today is';
+    var str2 = ' A beautiful day '
+    var str3 = ' In Hawaii. '
+    Result = 'Today is a beautiful day in Hawaii.'
+
+// 1. Use the str1, str2, str3 to create the Result string. Keep the extra spaces, lowercase and uppercase letters in mind.
+var reformattedStr2 = (str2.toLowerCase()).trim(); 
+var reformattedStr3 = (str3.toLowerCase()).trim();
+
+var myResult = str1.concat(' ' + reformattedStr2 + ' ' + reformattedStr3);
+console.log(myResult);
+
+// 2. Convert this string to an array that holds different days. Also, capitalize all the characters.
+    var mStr = "Mo Tu We Th Fr Sa Su";
+
+var mStrUpperCase = mStr.toUpperCase();
+var mStrDays = mStrUpperCase.split(' ');
+console.log(mStrDays);
 
 
