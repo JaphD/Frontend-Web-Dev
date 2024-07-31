51374b68e702caf5ab2fd453dfc4ec6f26414e96
Yafet Daniel Haileselassie <japhethdanny@gmail.com>
2024-07-31 13:12:46 +0300

    Null and Undefined Values

---
+++ JavaScript/Null and Undefined Values.js
@@ -0,0 +1,14 @@
+var mVar // mVar hold no value.
+console.log(mVar);
+
+mVar = null;
+console.log(mVar); //mVar holds the value null
+
+console.log('');
+
+// Practice Problem
+// 1. var mNum; console.log(mNum) - nNum holds no value or it is undefined
+// 2. var mNum = null; console.log(mNum) - nNum holds a null value 
+
+// 3. console.log(undefined == null) - '==' operator compares two values for equality after converting both values to a common type (type coercion).
+// 4. console.log(undefined === null) - '===' operator compares both the value and the type without performing any type conversion.
