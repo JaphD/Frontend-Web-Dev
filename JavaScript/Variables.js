8f49e564c95fa7963c27df3a45a57cfab05878f5
Yafet Daniel Haileselassie <japhethdanny@gmail.com>
2024-07-31 10:04:01 +0300

    Variables

---
+++ JavaScript/Variables.js
@@ -0,0 +1,40 @@
+
+var firstNum = 10;
+console.log(firstNum);
+
+var secondNum = 35;
+console.log(secondNum);
+
+// Camel case (Recommended for JS)
+var firstNum;
+
+// Pascal case
+var FirstNum;
+
+// Snake case
+var first_num;
+
+//Rules for naming JS variables
+
+// - Name can contain letters, digits, _ and $
+var mysubject1, math$
+// - Name cannot start with a digit
+//var 1maths;
+// - Name can start with an _ or $
+var _subjectmarks, $rankInClass;
+// - Names cannot be reserved keywords
+//var var;
+// - Names are case sensitive
+var x, X;
+
+// Practice Problem
+// 1. &alpha1 = Invalid because a variable can't start with a '&' symbol
+// 2. NUM1 = Valid but doesn't follow camel case
+// 3. DATE-5 = Invalid because a variable can't contain a '-'
+// 4. TotalSum_ = Valid but doesn't follow camel case
+// 5. 12Byby = Invalid because a variable can't start with a digit
+// 6. num1 = Valid
+// 7. super$$ = Valid
+// 8. Object = Invalid because a variable name can't be a reserved keyword
+// 9. StringNew = Valid
+// 10. $rankInClass = Valid
