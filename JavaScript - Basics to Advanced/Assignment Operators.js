edd09cdfcf7b59e3424a9984da48ed2fbdab8f8e
Yafet Daniel Haileselassie <japhethdanny@gmail.com>
2024-08-01 18:03:26 +0300

    Assignment Operators

---
+++ JavaScript - Basics to Advanced/Assignment Operators.js
@@ -0,0 +1,40 @@
+var num1 = 15;
+var num2 = 30;
+
+// =
+num1 = 20;
+
+// +=
+num1 += 10; // num1 = num1 + 10
+num1 += num2; // num1 num1 + num2
+console.log(num1);
+
+// -=
+num1 -= 10; //num1 = num1 - 10
+num1 -= num2 //num1 = num1 - num2
+console.log(num1);
+
+// *=
+num1 *= 20 //num1 = num1 * 20
+console.log(num1)
+
+// /=
+num1 /= 10;
+console.log(num1);
+
+// %=
+num1 %= 10; //num1 = num1 % 10
+console.log(num1);
+
+// Practice Problems
+var number1 = 100;
+number1 += 5 * 10 / 2;
+console.log(number1)
+
+var number2 = 100;
+number2 *= 2 * 10 / 5;
+console.log(number2);
+
+var number3 = 100;
+number3 /= 2 * 8 - 15;
+console.log(number3);
