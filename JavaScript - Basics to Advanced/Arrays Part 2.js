ee0ed2aadd871e1730b6c05fb7dfd8091e091a85
Yafet Daniel Haileselassie <japhethdanny@gmail.com>
2024-08-13 22:02:07 +0300

    Added Practice Problems

--- JavaScript - Basics to Advanced/Arrays Part 2.js
+++ JavaScript - Basics to Advanced/Arrays Part 2.js
@@ -52,28 +52,106 @@ console.log('')
 
 //Q2. Write a JavaScript program to check if there is at least one element which occurs in
 //two given sorted arrays of integers.
-arr1 = [4, 11, 12, 23, 46, 65, 70, 73, 98];
-arr2 = [7, 13, 25, 46, 58, 70, 84];
+    arr1 = [4, 11, 12, 23, 46, 65, 70, 73, 98];
+    arr2 = [7, 13, 25, 46, 58, 70, 84];
 
-i = 0;
-j = 0;
-let commonElementFound = false;
+    i = 0;
+    j = 0;
+    let commonElementFound = false;
 
-while (i < arr1.length && j < arr2.length) {
-    if (arr1[i] == arr2[j]) {
-        commonElementFound = true;
-        break;
+    while (i < arr1.length && j < arr2.length) {
+        if (arr1[i] == arr2[j]) {
+            commonElementFound = true;
+            break;
+        }
+        else if (arr1[i] < arr2[j]) {
+            i++;
+        }
+        else {
+            j++;
+        }
     }
-    else if (arr1[i] < arr2[j]) {
-        i++;
+
+    console.log(commonElementFound ? "There is at least one common element." : "No common elements found.")
+
+    console.log('')
+
+//Q3. Write a JavaScript program to check whether a given array of integers represents
+//either a strictly increasing or a strictly decreasing sequence.
+    newArray1 = [5,4,3,2,1];
+
+    let isIncreasing = true;
+    let isDecreasing = true;
+    
+    
+    if (newArray1.length <= 1) {
+        console.log("Array must have at least two elements to check for a sequence.");
+    } else {
+        
+        for (let i = 1; i < newArray1.length; i++) {
+            if (newArray1[i] <= newArray1[i - 1]) {
+                isIncreasing = false;
+            }
+            if (newArray1[i] >= newArray1[i - 1]) {
+                isDecreasing = false;
+            }
+        }
+        if (isIncreasing) {
+            console.log("The array is strictly increasing.");
+        } else if (isDecreasing) {
+            console.log("The array is strictly decreasing.");
+        } else {
+            console.log("The array is neither strictly increasing nor strictly decreasing.");
+        }
+    } 
+
+    console.log('')
+
+//Q4. Write a JavaScript function to convert an amount to coins.
+    amount = 45;
+    coins = [25, 10, 5, 2, 1];
+
+    result = [];
+
+    for (let i = 0; i < coins.length; i++) {
+        while (amount >= coins[i]) {
+            amount -= coins[i];
+            result.push(coins[i]);
+        }
     }
-    else {
-        j++;
+
+    console.log(result)
+
+    console.log('')
+
+//Q5. Write a JavaScript function to create a new array from given array by formatting the
+//numbers to human readable form with correct suffix like 1 to 1st, 2 to 2nd, 3 to 3rd and
+//4 to 4th.
+    let numbers = [22,8, 301, 404, 35, 99];
+    let formattedNumbers = [];
+
+    for (let i = 0; i < numbers.length; i++) {
+        let num = numbers[i];
+        let suffix = "th";
+
+        if (num % 100 >= 11 && num % 100 <= 13) {
+            suffix = "th";
+        } else {
+            switch (num % 10) {
+                case 1:
+                    suffix = "st";
+                    break;
+                case 2:
+                    suffix = "nd";
+                    break;
+                case 3:
+                    suffix = "rd";
+                    break;
+            }
+        }
+
+        formattedNumbers.push(num + suffix);
     }
-}
 
-console.log(commonElementFound ? "There is at least one common element." : "No common elements found.")
-
-console.log('')
-
-//
+    console.log(formattedNumbers); 
+    
