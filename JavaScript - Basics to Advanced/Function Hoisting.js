b9da029b02ece538382bcf4104ceccc40aeff4d0
Yafet Daniel Haileselassie <japhethdanny@gmail.com>
2024-08-15 11:37:25 +0300

    Function Hoisting

---
+++ JavaScript - Basics to Advanced/Function Hoisting.js
@@ -0,0 +1,13 @@
+
+//sum(5,10);
+//sum(14,25)
+var total = 10;
+let sum = function sum(num1, num2) {
+    console.log(total)
+    total = num1 + num2;
+    var total;
+    console.log(total);
+}
+
+sum(90, 100)
+
