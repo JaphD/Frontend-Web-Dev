0e394b23b2a37a1fa5fcdae598751a0735f4a63e
Yafet Daniel Haileselassie <japhethdanny@gmail.com>
2024-08-15 11:24:40 +0300

    Variable Hoisting

---
+++ JavaScript - Basics to Advanced/Variable Hoisting.js
@@ -0,0 +1,13 @@
+num1 = 10;
+console.log(num1);
+var num1;
+
+str1 = 'Demo String';
+console.log(str1);
+var str1;
+
+// We can see variable hoisting in full effect below
+console.log(yafet)
+yafet = 22;
+console.log(yafet)
+var yafet;
