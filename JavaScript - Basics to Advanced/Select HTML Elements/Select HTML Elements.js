bd6b2d1a64ab05113e538e2c4744ca167d07f29d
Yafet Daniel Haileselassie <japhethdanny@gmail.com>
2024-08-19 16:15:21 +0300

    Selecting HTML Elements

---
+++ JavaScript - Basics to Advanced/Select HTML Elements/Select HTML Elements.js
@@ -0,0 +1,7 @@
+console.log('Script Loaded')
+
+console.log(document.getElementById("topbar"))
+
+console.log(document.getElementsByClassName('heading'))
+
+console.log(document.getElementsByTagName('article')) 
