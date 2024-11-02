a825eb9fd5135eb918e9ea8a7ae765384372db97
Yafet Daniel Haileselassie <japhethdanny@gmail.com>
2024-11-02 11:27:35 +0300

    Mouse Events
    Tested how the different mouse events work

---
+++ JavaScript - Basics to Advanced/Mouse Events/Mouse.js
@@ -0,0 +1,44 @@
+var btn = document.getElementById('click');
+
+btn.addEventListener('mousedown', function(){
+    console.log('Mouse Down Event')
+})
+
+btn.addEventListener('mouseup', function(){
+    console.log('Mouse Up Event')
+})
+
+btn.addEventListener('click', function(){
+    console.log('Click')
+})
+
+btn.addEventListener('dblclick', function(){
+    console.log('Double Click Event')
+})
+
+var over = document.getElementById('over');
+
+var overCount = 0;
+over.addEventListener('mouseover', function(){
+    var countElement = document.querySelector('#over > p');
+    overCount += 1;
+    countElement.innerHTML = overCount;
+})
+
+var enter = document.getElementById('enter');
+
+var enterCount = 0;
+enter.addEventListener('mouseenter', function(){
+    var countElement = document.querySelector('#enter > p');
+    enterCount += 1;
+    countElement.innerHTML = enterCount;
+})
+
+var moveElement = document.getElementById('move');
+
+var moveCount = 0;
+move.addEventListener('mousemove', function(){
+    var countElement = document.querySelector('#move > p');
+    moveCount += 1;
+    countElement.innerHTML = moveCount;
+})
